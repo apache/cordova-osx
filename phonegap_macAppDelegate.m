52c390fe6bc53069cd85b154a6031c0c99c4a905
RandyMcMillan <randy.lee.mcmillan@gmail.com>
2011-07-12 04:43:18 -0400

    Creating NSColor Strings so dev can control Colors in application window & contentView
    Creating NSColor Strings so dev can control Colors in application window & contentView

--- phonegap_macAppDelegate.m
+++ phonegap_macAppDelegate.m
@@ -23,7 +23,25 @@
 	
     NSURL* fileUrl = [NSURL fileURLWithPath:[[Utils sharedInstance] pathForResource:kStartPage]];
 	[self.contentView.webView setMainFrameURL:[fileUrl description]];
+    
+    /* useful colors
+    NSColor *red = [NSColor colorWithCalibratedRed:0.918 green:0.000 blue:0.000 alpha:1.000];
+    NSColor *orange = [NSColor colorWithCalibratedRed:0.918 green:0.518 blue:0.000 alpha:1.000];
+    NSColor *yellow = [NSColor colorWithCalibratedRed:0.918 green:0.914 blue:0.000 alpha:1.000];
+    NSColor *green = [NSColor colorWithCalibratedRed:0.000 green:0.918 blue:0.106 alpha:1.000];
+    NSColor *skyBlue = [NSColor colorWithCalibratedRed:0.000 green:0.918 blue:0.906 alpha:1.000];
+    NSColor *blue = [NSColor colorWithCalibratedRed:0.000 green:0.020 blue:0.918 alpha:1.000];
+    NSColor *clearColor = [NSColor clearColor];
+    */
+    
+    NSColor *windowBackGroundColor = [NSColor colorWithCalibratedRed:0.918 green:0.918 blue:0.918 alpha:1.000];
+    NSColor *webViewBackGroundColor = [NSColor colorWithCalibratedRed:0.082 green:0.133 blue:0.192 alpha:1.000];
+    [window setBackgroundColor:windowBackGroundColor];
+    [self.contentView.webView setBackgroundColor:webViewBackGroundColor]; //it ignores "WebView" may not respond to 'setBackGroundColor:' @RandyMcMillan
     self.window.backgroundColor = [NSColor colorWithCalibratedRed:0.933 green:0.933 blue:0.933 alpha:1.000];
+    
+    
+    
 }
 
 - (void) applicationDidFinishLaunching:(NSNotification *)aNotification {
