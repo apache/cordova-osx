297d580f8dbcd4b4552e748ffb4de52fbbda9c7a
Tobias Bocanegra <tripod@adobe.com>
2014-04-25 00:47:44 -0700

    CB-6516 (re) add missing test harness
    fixed
    - added TestApp
    - added some initial test cases
    - added bin/tests to execute the tests from the command line

--- CordovaLib/CordovaLibTests/TestApp/MainViewController.h
+++ CordovaLib/CordovaLibTests/TestApp/MainViewController.h
@@ -6,9 +6,9 @@
  to you under the Apache License, Version 2.0 (the
  "License"); you may not use this file except in compliance
  with the License.  You may obtain a copy of the License at
-
+ 
  http://www.apache.org/licenses/LICENSE-2.0
-
+ 
  Unless required by applicable law or agreed to in writing,
  software distributed under the License is distributed on an
  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -17,29 +17,16 @@
  under the License.
  */
 
-#import <XCTest/XCTest.h>
+#import <Cordova/CDVViewController.h>
+#import <Cordova/CDVCommandDelegateImpl.h>
+#import <Cordova/CDVCommandQueue.h>
 
-@interface CordovaLibTests : XCTestCase
+@interface MainViewController : CDVViewController
 
 @end
 
-@implementation CordovaLibTests
+@interface MainCommandDelegate : CDVCommandDelegateImpl
+@end
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
-
+@interface MainCommandQueue : CDVCommandQueue
 @end
